a7ffa9bfaa1a521ffc89ec675bf746b61807cd94
Aiden Qian <aidenzqian@gmail.com>
2026-07-05 10:40:12 -0500

    Add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI(title="ServeIQ API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:19006", "http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://serve-iq-xxxx.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
